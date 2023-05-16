2dd49a095cae6e4ffed16a2c262a2d119d88f900
Samantha Crespo <samkcrespo@gmail.com>
2023-05-16 15:16:55 -0700

    add home page image

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ end
 gem "active_model_serializers", "~> 0.10.12"
 
 gem "aws-sdk-s3", "~> 1.119"
+
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,13 +3,16 @@ import jdmLogo from '../images/JdmLogoWhite.png';
 import Slideshow from './Slideshow';
 import transparentcars from "../images/transparentcars.png";
 
-
 function Home() {
   return (
     <div>
-    <img src={transparentcars} alt="JDM Import House Garage"/>
+    <div>
+      <img src={transparentcars} alt="JDM Import House Garage" style={{ width: '100%' }}  />
+      </div>
     <Slideshow />
     </div>
+
+    
   )
 }
 
