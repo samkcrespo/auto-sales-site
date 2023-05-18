454bee0f78e30730d6e351063d393987a684e3c7
Samantha Crespo <samkcrespo@gmail.com>
2023-05-18 13:39:54 -0700

    Add about copy

---
+++ client/src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <div>
+      JDM Import House was founded by Adrian Zambrana in 2020. 
+      Adrian had a passion for cars ever since he was a kid and was also a business man at heart. 
+      Even as a kid, he was always figuring out creative ways to drum up some business. 
+      He started working on and selling cars when he was just a teenager, and through out the years Adrian became immersed in the car scene and made connections all over the US and Japan.
+      Once Adrian was later into his college years, he decided he wanted to take his passion for cars and business to the next level and open his own shop. 
+      He opened his shop in 2020, and in a short time, grew it to be a well known and reputable company that has distributed hundreds of beautiful and rare JDM vehicles all over the United States. 
+      Adrian loved making it possible for people to have the car of their dreams. 
+Adrian put 110% into everything that he did. When he passed he had just graduated with a bachelors in business and was getting ready to go to France to get his masters in international business. 
+He had grown JDM Import House rapidly and when he passed he had 13 cars in his inventory and more on the way from Japan. 
+His family decided that they had to keep Adrian's dream business going in his memory and to keep serving the car community he loved. 
+Adrian's parents, Claudia and Sergio, and his brother Renato (pictured to the right) have kept the business growing in his memory, along with the help of his cousin Andrew, who Adrian got into cars when they were kids (pictured on the right, working on one of Adrian's first cars ever), as well as friends that Adrian made in the car community that stepped up to help his family keep what Adrian built going.
+Today, JDM Import House is thriving and working with a number of reputable suppliers from Japan to keep putting people in their dream cars.
+        </div>
+        <div>
+        </div>
+    </div>
+  )
+}
+
+export default About
