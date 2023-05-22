4bf609379eae69a71b802c3d1917dacf09f96cd8
Samantha Crespo <samkcrespo@gmail.com>
2023-05-22 15:50:45 -0700

    add H3 to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ import transparentcars from "../images/transparentcars.png";
 function Home() {
   return (
     <div>
-    <div>
+    <div><h3>Your Home for Genuine Japanese Cars</h3>
    <img src={transparentcars} alt="JDM Import House Garage" style={{ width: '100%', position: 'absolute', top: 0, left: 0, zIndex: -1  }} />
       </div>
         {/* <div style={{ position: 'relative', zIndex: 4 }}>   
